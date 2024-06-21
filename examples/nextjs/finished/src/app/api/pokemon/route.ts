1f7a951fb05593d57c68067f15d5f64d7989ac8a
Christoph Wolf <Christoph.Wolf1988@gmail.com>
2024-06-21 13:02:28 +0200

    update example

--- examples/nextjs/finished/src/app/api/pokemon/route.ts
+++ examples/nextjs/finished/src/app/api/pokemon/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { safeParseSearchParams } from "routes/utils"
+import { safeParseSearchParams } from "@/routes/utils"
 import { Route } from "./route.info"
 import { getFullPokemon } from "@/pokemon";
 
@@ -8,7 +8,7 @@ export const dynamic = "force-dynamic";
 export async function GET(req: NextRequest) {
   const url = new URL(req.url);
   const searchParams = safeParseSearchParams(Route.search, req.nextUrl.searchParams)
-  const q = url.searchParams.get("q") ?? "";
-  const limit = url.searchParams.get("limit") ?? 10;
+  const q = searchParams.q ?? "";
+  const limit = searchParams.limit ?? 10;
   return NextResponse.json(await getFullPokemon(+limit, q));
 }
